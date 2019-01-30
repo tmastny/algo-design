ffe07011963f06b91cdca3edf513361538b82e7a
Timothy Mastny <tim.mastny@gmail.com>
2019-01-30 06:14:21 -0600

    refactoring bin search

--- chapter-3/exercises/3-10/bin_packing.py
+++ chapter-3/exercises/3-10/bin_packing.py
@@ -8,26 +8,43 @@ class Bins(Tree):
     def __init__(self, weight = None):
         super().__init__(weight)
 
+    def put_in_smaller_bin(self, weight):
+        if self.head.left == None:
+            self.head.left = Bins()
+        return(self.head.left.best_fit(weight))
+
+    def put_in_this_bin(self, weight):
+            self.head.data += weight
+            return()
+
     def best_fit(self, weight):
+        # this method currently does not maintain tree order.
+        # This method searchs for the right node to bin the weight,
+        # but after adding to the bin, the previous tree order could be
+        # broken.
+        #
+        # Therefore, after the right node is found, the node must be deleted
+        # and a new node with the new weight must be insert to maintain tree
+        # order.
+        #
+        # This procedure maintans O(log(n)) operations.
+
         if self.head.data == None:
             self.head.data = weight
             return()
 
         this_bin_capacity = weight + self.head.data
         if this_bin_capacity > 1:
-            if self.head.left == None:
-                self.head.left = Bins()
-
-            return(self.head.left.best_fit(weight))
+            return(self.put_in_smaller_bin(weight))
 
         if self.head.right == None:
-            self.head.data = this_bin_capacity
+            self.put_in_this_bin(weight)
             return()
 
         next_bin_capacity = weight + self.head.right.data
 
         if next_bin_capacity > 1:
-            self.head.data = this_bin_capacity
+            self.put_in_this_bin(weight)
             return()
 
         if next_bin_capacity > this_bin_capacity:
@@ -57,6 +74,6 @@ if __name__ == '__main__':
     print(' '.join(["{0:0.2f}".format(i) for i in weights]))
 
     print(pack_bins(weights))
-    print(pack_bins(weights))
+    #print(pack_bins(weights))
 
     print(sum(weights))
