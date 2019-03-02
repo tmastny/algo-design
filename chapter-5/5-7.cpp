78cf6e39bc3fb17793c79288d466343646f9cf76
Timothy Mastny <tim.mastny@gmail.com>
2019-03-01 20:56:52 -0600

    saving algo

--- chapter-5/5-7.cpp
+++ chapter-5/5-7.cpp
@@ -19,27 +19,101 @@
 
 using namespace std;
 
-void print_graph()
+class Graph {
+  public:
+
+    map<string, set<string>> g;
+
+    set<string>& operator[] (string& node) {
+      return g[node];
+    }
+
+    void print() {
+      for (auto it = g.begin(); it != g.end(); it++) {
+        cout << it->first << endl;
+      }
+    }
+
+    void print_set() {
+      for (auto it = g.begin(); it != g.end(); it++) {
+        cout << it->first << ": ";
+        for (auto it2 = it->second.begin(); it2 != it->second.end(); it2++) {
+          cout << *it2 << " ";
+        }
+        cout << endl;
+      }
+    }
+};
+
+Graph build_tree(map<string, deque<string>>& orders) {
+  Graph g;
+  deque<string> pre = orders["pre"];
+  deque<string> in = orders["in"];
+
+  for (auto i : pre) {
+    set<string> init;
+    g[i] = init;
+  }
+
+  int i = 0;
+  int j = 0;
+  while (pre[i] != in[j]) {
+    i++;
+  }
+
+  for (int i = 0; i < pre.size(); i++) {
+    if (i < pre.size() - 1) {
+      g[pre[i]].insert(pre[i + 1]);
+    }
+    for (int j = 0; j < in.size(); j++) {
+      if (pre[i] == in[j]) {
+
+      }
+    }
+  }
+}
 
 int main() {
-  map<string, set<string>> graph;
+  Graph graph;
   int nodes;
-  cin >> nodes;
+  cin >> nodes >> ws;
 
   for (int i = 0; i < nodes; i++) {
     string node;
-    cin >> node;
+    cin >> node >> ws;
 
     string line;
     getline(cin, line);
+
     istringstream iss(line);
 
     string adj;
     set<string> adj_nodes;
     while (iss >> adj) {
-      adj_nodes.insert(adj);
+      if (adj != "NULL") {
+        adj_nodes.insert(adj);
+      }
     }
 
-    graph.insert(make_pair(node, adj_nodes));
+    graph[node] = adj_nodes;
+  }
+  graph.print_set();
+
+  map<string, deque<string>> orders;
+  for (int i = 0; i < 3; i++) {
+    string order_type;
+    cin >> order_type >> ws;
+
+    string line;
+    getline(cin, line);
+    istringstream iss(line);
+
+    string node;
+    deque<string> nodes;
+    while (iss >> node) {
+      nodes.push_back(node);
+    }
+
+    orders[order_type] = nodes;
   }
 }
